7aac801aa0dfcd72f3a40c35d28f72deecb26891
sugyan <sugi1982@gmail.com>
2015-10-07 23:18:17 +0900

    use github.com/kurrik/twittergo

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/ChimeraCoder/anaconda"
+	"github.com/kurrik/oauth1a"
+	"github.com/kurrik/twittergo"
 	"log"
-	"net/url"
+	"net/http"
+	"os"
 )
 
 func init() {
@@ -11,14 +13,23 @@ func init() {
 }
 
 func main() {
-	// TODO: config...
-	anaconda.SetConsumerKey("")
-	anaconda.SetConsumerSecret("")
-	api := anaconda.NewTwitterApi("", "")
-	v := url.Values{}
-	user, err := api.GetSelf(v)
+	clientConfig := &oauth1a.ClientConfig{
+		ConsumerKey:    os.Getenv("CONSUMER_KEY"),
+		ConsumerSecret: os.Getenv("CONSUMER_SECRET"),
+	}
+	userConfig := oauth1a.NewAuthorizedConfig(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+	client := twittergo.NewClient(clientConfig, userConfig)
+
+	req, err := http.NewRequest("GET", "/1.1/account/verify_credentials.json", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	resp, err := client.SendRequest(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := &twittergo.User{}
+	resp.Parse(user)
 	log.Println(user)
 }
