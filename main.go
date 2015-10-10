152adc568dc2156ebc0658b555a5476c1533dda5
sugyan <sugi1982@gmail.com>
2015-10-11 01:35:25 +0900

    use replybot

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"github.com/kurrik/oauth1a"
 	"github.com/kurrik/twittergo"
+	"github.com/sugyan/replybot"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 )
 
@@ -14,66 +14,31 @@ func init() {
 }
 
 func main() {
-	bot, err := getBotAccount()
+	var (
+		consumerKey       = os.Getenv("CONSUMER_KEY")
+		consumerSecret    = os.Getenv("CONSUMER_SECRET")
+		accessToken       = os.Getenv("ACCESS_TOKEN")
+		accessTokenSecret = os.Getenv("ACCESS_TOKEN_SECRET")
+	)
+	account, err := getAccount(consumerKey, consumerSecret, accessToken, accessTokenSecret)
 	if err != nil {
 		log.Fatal(err)
 	}
-	clientConfig := &oauth1a.ClientConfig{
-		ConsumerKey:    os.Getenv("CONSUMER_KEY"),
-		ConsumerSecret: os.Getenv("CONSUMER_SECRET"),
+
+	bot := replybot.NewBot(consumerKey, consumerSecret)
+	ids, err := bot.FollowersIDs(account.IdStr())
+	if err != nil {
+		log.Fatal(err)
 	}
-	client := twittergo.NewClient(clientConfig, nil)
-
-	var count int
-	var cursor string
-	for {
-		query := url.Values{}
-		query.Set("user_id", bot.IdStr())
-		query.Set("stringify_ids", "true")
-		query.Set("count", "5000")
-		if cursor != "" {
-			query.Set("cursor", cursor)
-		}
-		req, err := http.NewRequest("GET", "/1.1/followers/ids.json?"+query.Encode(), nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-		res, err := client.SendRequest(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		results := &CursoredIDs{}
-		if err := res.Parse(results); err != nil {
-			log.Fatal(err)
-		}
-
-		log.Println(results)
-		for _, id := range results.IDs() {
-			count++
-			log.Println(id)
-		}
-
-		log.Printf("%v, %v", results.PreviousCursorStr(), results.NextCursorStr())
-		if res.HasRateLimit() {
-			log.Printf("Rate Limit: %v / %v, (next reset: %v)\n", res.RateLimitRemaining(), res.RateLimit(), res.RateLimitReset())
-		}
-
-		if results.NextCursorStr() == "0" {
-			break
-		} else {
-			cursor = results.NextCursorStr()
-		}
-	}
-	log.Printf("%v users", count)
+	log.Println(len(ids))
 }
 
-func getBotAccount() (user *twittergo.User, err error) {
+func getAccount(consumerKey string, consumerSecret string, accessToken string, accessTokenSecret string) (*twittergo.User, error) {
 	clientConfig := &oauth1a.ClientConfig{
-		ConsumerKey:    os.Getenv("CONSUMER_KEY"),
-		ConsumerSecret: os.Getenv("CONSUMER_SECRET"),
+		ConsumerKey:    consumerKey,
+		ConsumerSecret: consumerSecret,
 	}
-	userConfig := oauth1a.NewAuthorizedConfig(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+	userConfig := oauth1a.NewAuthorizedConfig(accessToken, accessTokenSecret)
 	client := twittergo.NewClient(clientConfig, userConfig)
 
 	req, err := http.NewRequest("GET", "/1.1/account/verify_credentials.json", nil)
@@ -84,7 +49,7 @@ func getBotAccount() (user *twittergo.User, err error) {
 	if err != nil {
 		return nil, err
 	}
-	user = &twittergo.User{}
+	user := &twittergo.User{}
 	res.Parse(user)
 
 	return user, nil
